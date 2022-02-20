07dbdb43034588191472b442cc657e68c7156c90
PhongCiber <86000885+PhongCiber@users.noreply.github.com>
2022-02-21 01:13:39 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Chào c iu !',
+    introDesc: `ngồi trong toilet
+                gào thét tên em'      
+    btnIntro: 'click đi quý cô xinh xắn' 
+    title: 'E chào c lần đầu e đi tỏ tình nên là có gì sai sót mong c bỏ qua nhaaaa',
+    desc: '2 tuần rùi đó c ạ nhanh nhỉ ờm hôm nay là ngày 22/02/2022 là ngày người ta bảo là 
+    có nhiều năng lượng tích cực lắm lun ấy thì e có vấn đề này hỏi c 😊 ',
+    btnYes: 'Vẫn cứ là thích eee <33',
+    btnNo: 'Không, Em trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao c lại yêu e <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'e biết mà 🥰. Yêu c nhiều nhiều 😘😘',
+    messLink: 'http://fb.com' https://www.facebook.com/m.phongdepzai/
+}
